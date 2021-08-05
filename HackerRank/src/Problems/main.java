5bc7d1a9654c47b7142cb81f36696e30e5049dab
Billski <bill@bill-tucker.com>
2021-08-05 14:25:19 -0700

    Java If-Else

---
+++ HackerRank/src/Problems/main.java
@@ -0,0 +1,70 @@
+package Problems;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Problem2();
+
+	}
+	
+	
+	static void Problem2() {
+		
+		/*
+		 	Given an integer, n, perform the following conditional actions:
+
+			If n is odd, print Weird
+			If n is even and in the inclusive range of  to , print Not Weird
+			If n is even and in the inclusive range of  to , print Weird
+			If n is even and greater than , print Not Weird
+		*/
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		
+		if(num <= 100 && num >= 1) {
+			if(num % 2 == 0) {
+				if((num >= 2 && num <= 5)) {
+					System.out.println("Not Weird");
+				}else if ( (num >= 6 && num <= 20)) {
+					System.out.println("Weird");
+				}else if(num > 20) {
+					System.out.println("Not Weird");
+				}
+				
+			} else {
+				System.out.println("Weird");
+			}
+			
+		}else {
+			System.out.println("Invalid number.");
+			
+		}
+		
+		
+		
+		
+	}
+	
+	
+	static void Problem1() {
+		Scanner scanner = new Scanner(System.in);
+		Integer ints[] = new Integer[3];  
+		
+		for(int i = 0, j = 0; i < ints.length || j < ints.length;) {
+		
+			
+			if(i < ints.length) {
+				ints[i] = scanner.nextInt();
+				i++;
+			} 
+			if(i == ints.length) {
+				System.out.println(ints[j]);
+				j++;				
+			}
+		}
+	}
+
+}
