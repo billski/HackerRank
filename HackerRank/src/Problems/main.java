b16701e6dc3b61be74dfe1b2f8514b51157033ba
Billski <bill@bill-tucker.com>
2021-08-05 20:14:50 -0700

    Java Datatypes

--- HackerRank/src/Problems/main.java
+++ HackerRank/src/Problems/main.java
@@ -9,10 +9,39 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		Problem6();
+		Problem7();
 	}
     
+	static void Problem7() {
 	
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		for(int i = 0; i < t; i++){
+		
+			try {
+				long x = sc.nextLong();
+				System.out.println(x + " can be fitted in:");
+				if(x >= -128 && x <= 127) {
+					System.out.println("* byte");
+				}
+				if(x == (short) x) {
+					System.out.println("* short");
+				}
+				if(x == (int) x) {
+					System.out.println("* int");
+				}
+				if(x == (long) x) {
+					System.out.println("* long");
+				}
+				
+			} catch(Exception e) {
+				System.out.println(sc.next() + " can't be fitted anywhere.");
+			}
+		}
+				
+	}
+		
 	static void Problem6() {
 		Scanner in = new Scanner(System.in);
 		int t = in.nextInt();
